effad0adf3471955c6f62e8949cb69407719cdf2
ceewizz <ceewizzhuhwin@gmail.com>
2024-01-22 21:57:00 -0800

    completed all the necessary files of controllers , models , javascript, stylesheet

---
+++ utils/auth.js
@@ -0,0 +1,13 @@
+// User authentication
+const withAuth = (req, res, next) => {
+    console.log(req.session.logged_in);
+
+    if (!req.session.logged_in) {
+      res.redirect("/login");
+    } else {
+      next();
+    }
+  };
+  
+  // Export
+  module.exports = withAuth;
